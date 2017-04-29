20901517c96c8a3344a5fac853b575204a840e5f
Peter <humkepm@gmail.com>
2017-04-28 22:42:09 -0700

    Expanded functionality for generating multiple heros, began refactoring string constants out, added a package.json.

--- gfff_bot.js
+++ gfff_bot.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const token = require('./chat_token.js');
+const input = require('./input_constants.js')
 
 const client = new Discord.Client();
 client.login(token);
@@ -8,18 +9,55 @@ client.on('ready', () => {
   console.log('I am ready!');
 });
 
+// TODO convert this to a string -> fn map instead
 client.on('message', message => {
   var content = message.content.toLowerCase();
 
-  if (content === 'help') {
-    message.reply('Commands: mh, my hero, brett likes men');
-  } else if (content === 'my hero' ||
-            content === 'mh') {
+  console.log(content);
+
+  if (content === input.HELP) {
+    message.reply('Commands: mh, my hero, brett likes men, brett loves dicks, our <count> heros, oh#');
+  } else if (content === input.MY_HERO ||
+            content === input.MY_HERO2) {
     message.reply(getRandomHero());
-  } else if (content === 'brett likes men') {
+  } else if (content === input.BRETT_LIKES_MEN ||
+            content === input.BRETT_LOVES_DICKS ||
+            (content.includes(input.BRETT) && content.includes(input.MEN))) {
     message.reply('(Always), and you\'re ' + getRandomHero());
+  } else if (content.includes(input.OUR) &&
+            content.includes(input.HEROS)) {
+    var count = parseInt(content.split(' ')[1]);
+    if (!isNaN(count)) {
+      message.reply(getRandomHeros(count).join(', '));
+    } else if (isNaN(count) && content.split(' ').length <= 3) {
+      message.reply('Please include a count, like our 3 heros');
+    }
+  } else if (content.startsWith(input.OH)) {
+    var count = parseInt(content[2]);
+    if (!isNaN(count)) {
+      message.reply(getRandomHeros(count).join(', '));
+    } else {
+      message.reply('Please include a count, like oh3');
+    }
+  } else if (content === input.MY_WEENIS ||
+            content === input.WEENIS) {
+    message.reply(':eggplant:');
   }
 });
+
+function getRandomHeros(count) {
+  var heros = new Set();
+
+  for (i = 0; i < count; ++i) {
+    var hero = getRandomHero();
+    while (heros.has(hero)) {
+      hero = getRandomHero();
+    }
+    heros.add(hero);
+  }
+
+  return Array.from(heros);
+}
 
 function getRandomHero () {
   var herolist = ['Genji', 'McCree', 'Pharah', 'Reaper', 'Soldier', 'Sombra', 'Tracer', 'Bastion', 'Hanzo', 'Junkrat', 'Mei', 'Torbjorn', 'Widowmaker', 'D.Va', 'Orisa', 'Reinhardt', 'Roadhog', 'Winston', 'Zarya', 'Ana', 'Lucio', 'Mercy', 'Symmetra', 'Zenyatta'];
---
+++ input_constants.js
@@ -0,0 +1,14 @@
+module.exports = {
+  HELP: 'help',
+  BRETT_LIKES_MEN: 'brett likes men',
+  BRETT_LOVES_DICKS: 'brett loves dicks',
+  BRETT: 'brett',
+  MEN: 'men',
+  MY_HERO: 'my hero',
+  MY_HERO2: 'mh',
+  OUR: 'our',
+  HEROS: 'heros',
+  OH: 'oh',
+  MY_WEENIS: 'my weenis',
+  WEENIS: 'weenis',
+}
